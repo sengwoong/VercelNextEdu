d509be86ab34d4f78ca29f691d4ce69d963cba0d
sengwoong <92924243+sengwoong@users.noreply.github.com>
2023-05-04 14:07:58 +0900

    Update fetchMessages.ts
    change https

--- utils/fetchMessages.ts
+++ utils/fetchMessages.ts
@@ -2,11 +2,12 @@ import {Message}from "../typings"
 
 const fetcherMessages = async ()=>{
     const host = window.location.host;
-const res = await fetch(`http://${host}/api/getNewMessage`);
+const res = await fetch(`https://${host}/api/getNewMessage`);
     const data = await res.json()
     const messages: Message[] = data.messages;
 
     console.log("fetcherMessagesss")
+     console.log(res)
     return messages
 
 }
