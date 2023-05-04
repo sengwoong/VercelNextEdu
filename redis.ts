393b09eb5b362a29501949ffbcc235d2a6765072
sengwoong <rje287573@gmail.com>
2023-05-05 01:58:54 +0900

    tokenNotNull

--- redis.ts
+++ redis.ts
@@ -7,14 +7,12 @@ let redisClient: Redis | null = null;
 
 
 
-if (process.env.UPSTASH_REDIS_REST_TOKEN && process.env.UPSTASH_REDIS_REST_URL  ) {
+
   redisClient = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
   });
-} else {
-  console.error('REDIS_TOKEN environment variable is not set.');
-}
+
 
 export  const redis = redisClient;
 
