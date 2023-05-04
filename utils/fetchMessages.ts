707d7ba61fa938dca275ec6bcf1971619a395499
sengwoong <rje287573@gmail.com>
2023-05-04 23:46:10 +0900

    vercelFetchBug

--- utils/fetchMessages.ts
+++ utils/fetchMessages.ts
@@ -2,7 +2,7 @@ import {Message}from "../typings"
 
 const fetcherMessages = async ()=>{
   const protocol = window.location.protocol;
-  const host = window.location.host;
+  const host = window.location.host.split('/')[0];
   const res = await fetch(`${protocol}://${host}/api/getNewMessage`,
 {
     method: 'GET'
@@ -11,8 +11,10 @@ const fetcherMessages = async ()=>{
   
     const data = await res.json()
     const messages: Message[] = data.messages;
+    console.log(`${host}은==host`)
+    console.log( "window.location.host")
+    console.log( window.location.host)
 
-    console.log(res)
     return messages
 
 }
