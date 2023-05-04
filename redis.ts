779a26ce604cd02498739745ae6adbb2e77b3ec1
sengwoong <rje287573@gmail.com>
2023-05-05 03:25:12 +0900

    발악

--- redis.ts
+++ redis.ts
@@ -1,18 +1,19 @@
 
-
-
 import https from "https";
 import { Redis } from '@upstash/redis';
 
-export  const   redis = new Redis({
+const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL!,
   token:  process.env.UPSTASH_REDIS_REST_TOKEN!,
   agent: new https.Agent({ keepAlive: true })
 });
 
-(async () => {
-  const data = await redis.set('foo', 'bar');
-})();
+export default redis;
+
+
+
+
+
 
 // import https from "https";
 // import { Redis } from '@upstash/redis';
