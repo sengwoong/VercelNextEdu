8749f00fab98125f39cd7c04d9a4c2071f6419a9
sengwoong <rje287573@gmail.com>
2023-05-05 04:07:25 +0900

    공백제거

--- redis.ts
+++ redis.ts
@@ -2,10 +2,10 @@
 import https from "https";
 import { Redis } from '@upstash/redis';
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token:  process.env.UPSTASH_REDIS_REST_TOKEN!,
-  agent: new https.Agent({ keepAlive: true })
+const redis=new Redis({
+  url:process.env.UPSTASH_REDIS_REST_URL!,
+  token:process.env.UPSTASH_REDIS_REST_TOKEN!,
+  agent:new https.Agent({ keepAlive: true })
 });
 
 export default redis;
