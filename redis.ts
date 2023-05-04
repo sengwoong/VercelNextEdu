020db2c8d751e59525eac673104eee4aebec3a0c
sengwoong <rje287573@gmail.com>
2023-05-05 02:58:15 +0900

    redis환경변수 고침

--- redis.ts
+++ redis.ts
@@ -1,19 +1,34 @@
 
 
+
+import https from "https";
 import { Redis } from '@upstash/redis';
 
-let redisClient: Redis | null = null;
+export  const   redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token:  process.env.UPSTASH_REDIS_REST_TOKEN!,
+  agent: new https.Agent({ keepAlive: true })
+});
+
+(async () => {
+  const data = await redis.set('foo', 'bar');
+})();
+
+// import https from "https";
+// import { Redis } from '@upstash/redis';
+
+// export const redis = new Redis({
+
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+//   agent: new https.Agent({ keepAlive: true })
+// });
+
+// (async () => {
+//   const data = await redis.set('foo', 'bar');
+// })();
 
 
-
-
-
-  redisClient = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL!,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-  });
-
-
-export  const redis = redisClient;
+// url: process.env.UPSTASH_REDIS_REST_URL!,
+// token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 
 
