4f61838551bd19e47ce044df1977073194cdf591
sengwoong <92924243+sengwoong@users.noreply.github.com>
2023-05-04 14:08:31 +0900

    messages
    messages https

--- utils/fetchNewMessage.ts
+++ utils/fetchNewMessage.ts
@@ -2,10 +2,10 @@ import {Message}from "../typings"
 
 const fetcherMessage = async ()=>{
     const host = window.location.host;
-    const res = await fetch(`http://${host}/api/getNewMessage`);
+    const res = await fetch(`https://${host}/api/getNewMessage`);
     const data = await res.json()
     const messages: Message = data.messages;
-
+        console.log(res)
     console.log("fetcherMessagesss")
     return messages
 
